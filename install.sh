588113ca1d6e7c427a1fa3d0733e0167f9dbfdf5
Damien <damien@pobel.fr>
2020-12-09 14:16:25 +0100

    fix: add correct key for spotify

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo '# Installing packages'
 
 sudo apt install curl
 if [ ! -f  /etc/apt/sources.list.d/spotify.list ] ; then
-    curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+    curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 fi
 
