648ad5c4253c71cc0e13456fb87ee6c47b3c2f9b
Damien Pobel <damien@pobel.fr>
2025-01-23 10:30:09 +0100

    feat: update Inconsolata version

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ echo '# Installing Inconsolata Nerd Font'
 if [ ! -d ~/.local/share/fonts ] ; then
     mkdir -p ~/.local/share/fonts
     cd ~/.local/share/fonts
-    wget 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Inconsolata.zip' -O /tmp/Inconsolata.zip
+    wget 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Inconsolata.zip' -O /tmp/Inconsolata.zip
     unzip /tmp/Inconsolata.zip
     cd -
 fi
